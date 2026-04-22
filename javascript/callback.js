cdb1291347d61a05c7c277d7cd3703c1f8514ba1
luchos-git <trevissan.luciano@gmail.com>
2026-04-22 18:54:20 -0300

    callback1

---
+++ javascript/callback.js
@@ -0,0 +1,14 @@
+function Sumar(n1,n2){
+    return n1+n2;
+}
+
+function restar (n1,n2){
+    return n1-n2;
+} 
+
+function calcular (n1,n2, operacion){
+    return operacion(n1-n2);
+} 
+
+const resultado = calcular(10, 20, Sumar)
+console.log(resultado)
