c2699e66ff8ab880003d755ffe9c63cf5ada752c
luchos-git <trevissan.luciano@gmail.com>
2026-04-22 20:20:30 -0300

    array2

--- javascript/array.js
+++ javascript/array.js
@@ -3,3 +3,5 @@ let Semana =[`Lunes`, `Martes`, `Miercoles`, `Jueves`];
 // callback:
 console.log(Semana[0]);
 Semana.forEach(dia => {console.log(dia)});
+
+document.querySelector(`.texto`).innerHTML="nuevo Texto"
